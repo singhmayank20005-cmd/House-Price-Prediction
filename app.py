50cb9da444c5c68d27465cafeeb4fd817ff10f60
singhmayank20005-cmd <singhmayank20005@gmail.com>
2025-12-28 12:32:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import time
 from sklearn.datasets import fetch_california_housing
-st.title('🏠House Price prediction using ML')
+st.title('🏠House Price prediction using ML🏡')
 
 st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvQdqIasHkDTf5733FK14z5mPQ18VPhg_R_Q&s')
 
@@ -17,4 +17,13 @@ scaler = StandardScaler()
 scaled_X = scaler.fit_transform(final_X)
 
 st.sidebar.title('Select house features:')
+st.sidebar.image('https://cdn.dribbble.com/userupload/20000742/file/original-aaf23458355a156d0cf85b8217a5065a.gif')
+
+all_value=[]
+for i in final_X:
+  result=st.sidebar.slider(f'Select{i} value')
+  all_value.append(result)
+st.write(all_value)
+  
+
 
